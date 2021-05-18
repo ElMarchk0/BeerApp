dc511c840c69a1e716a4b5189cedd4d0c2f4f915
ElMarchk0 <67571255+ElMarchk0@users.noreply.github.com>
2021-05-17 21:39:24 -0700

    App:

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <Header :title="'The Great Victoria Beer Api'" />
+    <Search :search="state.search" @search="handleSearch" />
+    <div class="beers">
+      <Beer v-for="data in state.beer" beer="beer" :key="data.id"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// @ is an alias to /src
+import Header from './components/Header.vue'
+import Search from './components/Search.vue'
+import Beer from './components/Beer.vue'
+import { beerApi } from './hooks/beerApi.js'
 
 export default {
-  name: 'App',
+  name: 'Home',
   components: {
-    HelloWorld
+    Header,
+    Search,
+    Beer,
+  },
+
+  setup() {
+    const state = beerApi()
+
+      return {
+      state,
+      handleSearch(searchTerm) {
+        state.loading = true;
+        state.search = searchTerm;
+      }
+    };
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
