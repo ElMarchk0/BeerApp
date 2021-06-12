e818349f516c798c6bb471f61e1a703fa9ef3271
ElMarchk0 <67571255+ElMarchk0@users.noreply.github.com>
2021-06-12 00:50:02 -0700

    meh

--- src/hooks/beerApi.js
+++ src/hooks/beerApi.js
@@ -4,20 +4,21 @@ import axios from 'axios'
 const BEER_API_URL = `http://localhost:3000/beers`
 
 export const beerApi = () => {
-  const state = reactive({
-    search: null,
-    loading: true,
-    data: []
-  })
+    const state = reactive({
+        search: null,
+        loading: true,
+        data: []
+    })
 
-  watch(() => {
-    axios
-      .get(`${BEER_API_URL}?q=${state.search}`)
-      .then((response) => {        
-        state.data = response.data      
-        state.loading = false
-      })      
-  })
+    watch(() => {
+        axios
+            .get(`${BEER_API_URL}?q=${state.search}`)
+            .then((response) => {
+                state.data = response.data
+                state.loading = false
+                console.log(response)
+            })
+    })
 
-  return state
+    return state
 }
