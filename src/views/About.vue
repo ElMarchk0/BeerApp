b6eb0d540ca6401a629850dc2bda5c513b782d8f
Peter Charlesworth <petermc9011@gmail.com>
2021-06-24 18:49:58 -0700

    About fixed

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="mx-auto w-75 w-responsive" :adaptive="true" :resizable="true" style="padding-bottom: 2em">
     <b-card title="About" >
     <b-card-text>
-      The Victoria Beer API was inspired my love of crafft beer and a desire to learn web development. 
+      The Victoria Beer API was inspired my love of craft beer and a desire to learn web development. 
     </b-card-text>
   </b-card>
   </div>
