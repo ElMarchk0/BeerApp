54c2f176e397d33d461b0faa77e8f5d85a6f9db1
Peter Charlesworth <petermc9011@gmail.com>
2021-07-17 18:13:05 -0700

    upstream fixed

--- src/views/BeerView.vue
+++ src/views/BeerView.vue
@@ -1,35 +1,25 @@
 <template>
-  <div class="container-sm">
-    <div class="mx-auto w-50 d-inline-block w-responsive" :adaptive="true" :resizable="true" >
-      <div class="beerCard">
-        <div class="card" >
-          <div class="card-body">
-            <h3 class="card-title">{{ beer.name }}</h3>
-            <h5 class="card-title">{{ beer.brand }}</h5>
-            <h5 class="card-title">{{ beer.description }}</h5>
-            <h5 class="card-title">ABV: {{ beer.ABV }}</h5>
-            
-          </div>
+  <div class="col d-flex justify-content-center" >
+    <div class="beerCard">
+      <div class="card" style="width: 25rem">
+        <div class="card-body">
+          <h3 class="card-title">{{ beer.name }}</h3>
+          <h5 class="card-title">{{ beer.brand }}</h5>
+          <h5 class="card-title">{{ beer.description }}</h5>
+          <h5 class="card-title">{{ beer.ABV }}</h5>
         </div>
-        <div class="review">
-          <Review />        
-        </div>
-        
-      </div>    
-    </div>  
+      </div>
+    </div>
+    
   </div>
+  
 </template>
 
 <script>
-import Review from '../components/Review'
-
 export default {
   name: "BeerView",
-  props: ["beerId",'beers'], 
-  components: {
-    Review
-  },
-     
+  props: ["beerId",'beers'],
+
   mounted() {
     this.getSpecificBeer()
   },
@@ -38,15 +28,12 @@ export default {
       beer: {},
     };
   },
-  
   methods: {
     getSpecificBeer() {
       this.beer = this.beers.filter((beer) => beer.id == this.beerId)[0];
-      console.log(this.beers)      
+      console.log(this.beers)
     },
-    
-  },
-  
+  },  
 };
 </script>
 
@@ -54,8 +41,4 @@ export default {
   .card {
     margin-top: 2em;
   }
-  .review {
-    margin-top: 2em;
-  }
-  
 </style>
