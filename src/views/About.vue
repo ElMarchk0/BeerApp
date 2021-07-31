c9f1862aee0366ec8450f48b14bb8ee1bd5eae5d
Peter Charlesworth <petermc9011@gmail.com>
2021-07-31 14:34:41 -0700

    fixed another thing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="mx-auto h-50 d-inline-block h-responsive" :adaptive="true" :resizable="true" style="padding-top: 2em">
     <b-card title="About" >
     <b-card-text>
       The Victoria Beer API was inspired by my love of craft beer and a desire to learn web development. 
